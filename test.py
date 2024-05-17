f4075fd50f7f497c352023c9296be7edab6e8709
keba_e <fgdsgfsdg@hfjsd.fjgshdg>
2024-05-17 12:56:50 +0300

    Auto

--- test.py
+++ test.py
@@ -1,45 +1,50 @@
-# from auto import Auto
-# class Auto:
-#     engine = "DVS"
-#     wheels = 4
-#
-#     def __init__(self, marka):
-#         self.marka = marka
-#         self._speed = 0
-#     # def movement(self):
-#     #     self.speed += 10
-#     #     return self.speed
-# class Truck(Auto):
-#     # engine = "hybrid"
-#     def __init__(self):
-#         super().__init__(self)
-#         self.brand = "VOLVO"
-#     # def test(self):
-#     #     self.speed = 1400
-#
-#
-# t = Truck()
+from auto import Auto
+class Auto:
+    engine = "DVS"
+    wheels = 4
+    def __init__(self, accident):
+        self._speed = 0
+        self.accident = accident
+    def movement(self):
+        if self.accident != False:
+            self.speed += 10
+            return self.speed
+class Truck(Auto):
+    # engine = "hybrid"
+    def __init__(self, brand):
+        super().__init__(self)
+        self.brand = brand
+    def test(self):
+        self.speed = 1400
+
+
+t = Truck("Volvo")
+print(t.brand)
+print(t.accident)
 # t.speed = 150
 # print(t.speed)
 
 
-class Hello:
-    hello = "hello"
-    _hello = "_hello"
-    __hello = "__hello"
-
-    def __init__(self):
-        self.world = 'world'
-        self._world = '_world'
-        self.__world = '__world'
-
-class Hi(Hello):
-    def printer(self):
-        print(self.hello)
-        print(self._hello)
-        # print(self.__hello)
-        print(self.world)
-        print(self._world )
-        # print(self.__world)
-h = Hi()
-h.printer()
+# class Hello:
+#     hello = "hello"
+#     _hello = "_hello"
+#     __hello = "__hello"
+#
+#     def __init__(self):
+#         self.world = 'world'
+#         self._world = '_world'
+#         self.__world = '__world'
+#
+# class Hi():
+#     def __init__(self,a,b):
+#         self.__a = a
+#
+#     def __printer(self):
+#         print(self.hello)
+#         print(self._hello)
+#         # print(self.__hello)
+#         print(self.world)
+#         print(self._world)
+#         # print(self.__world)
+# h = Hi(5,2)
+# h.printer()
